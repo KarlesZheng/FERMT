c7d8fb36ecf2f9121d3da79c9bc86750142133ec
KarlesZheng <jikai_zheng@163.com>
2024-07-14 15:25:11 +0800

    Add files via upload

---
+++ lib/models/FERMT/DAU.py
@@ -0,0 +1,58 @@
+import torch.nn as nn
+
+class DualAttention(nn.Module):
+    """A Dual Attention block for Dual Attention Unit"""
+
+    def __init__(self, d_model, kernel_size=16, attn_shortcut=True):
+        super().__init__()
+        self.proj_1 = nn.Conv2d(d_model, d_model, 1)         # 1x1 conv
+        self.activation = nn.GELU()                          # GELU
+        self.spatial_gating_unit = DualAttentionModule(d_model, kernel_size)
+        self.proj_2 = nn.Conv2d(d_model, d_model, 1)         # 1x1 conv
+        self.attn_shortcut = attn_shortcut
+
+    def forward(self, x):
+        if self.attn_shortcut:
+            shortcut = x.clone()
+        x = self.proj_1(x)
+        x = self.activation(x)
+        x = self.spatial_gating_unit(x)
+        x = self.proj_2(x)
+        if self.attn_shortcut:
+            x = x + shortcut
+        return x
+    
+
+class DualAttentionModule(nn.Module):
+    """Large Kernel Attention for SimVP"""
+
+    def __init__(self, dim, kernel_size=16, dilation=3, reduction=16):
+        super().__init__()
+        d_k = 2 * dilation - 1
+        d_p = (d_k - 1) // 2
+        dd_k = kernel_size // dilation + ((kernel_size // dilation) % 2 - 1)
+        dd_p = (dilation * (dd_k - 1) // 2)
+
+        self.conv0 = nn.Conv2d(dim, dim, d_k, padding=d_p, groups=dim)
+        self.conv_spatial = nn.Conv2d(dim, dim, dd_k, stride=1, padding=dd_p, groups=dim, dilation=dilation)
+        self.conv1 = nn.Conv2d(dim, dim, 1)
+
+        self.reduction = max(dim // reduction, 3)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(dim, dim // self.reduction, bias=False), # reduction
+            nn.ReLU(True),
+            nn.Linear(dim // self.reduction, dim, bias=False), # expansion
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        u = x.clone()
+        attn = self.conv0(x)           # depth-wise conv
+        attn = self.conv_spatial(attn) # depth-wise dilation convolution
+        f_x = self.conv1(attn)         # 1x1 conv
+        # append a se operation
+        b, c, _, _ = x.size()
+        se_atten = self.avg_pool(x).view(b, c)
+        se_atten = self.fc(se_atten).view(b, c, 1, 1)
+        return se_atten * f_x * u
